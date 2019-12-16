30deb9ff2e82009c3f109e66a2853604e1de6eb6
최규진 <gyujin0450@naver.com>
2019-12-17 04:03:04 +0900

    MyBatis의 연결 테스트(p93~)

---
+++ exam01/src/test/java/kr/pmadvisor/web/MyBatisTest.java
@@ -0,0 +1,36 @@
+package kr.pmadvisor.web;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/**/*.xml"})
+public class MyBatisTest {
+	
+	@Autowired
+	private SqlSessionFactory sqlFactory;
+	
+	@Test
+	public void testFactory() {
+		System.out.println("sqlFactory : " + sqlFactory);
+	}
+	
+	@Test
+	public void testSession() {
+		
+		try(SqlSession session = sqlFactory.openSession()){
+			
+			System.out.println("session : " + session);
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}

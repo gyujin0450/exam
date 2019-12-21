96089cf35307188fb07018390f27d50bf71d3b43
최규진 <gyujin0450@naver.com>
2019-12-21 18:59:48 +0900

    스프링에 빈으로 등록하기(p136),테스트 코드 작성(p137)

--- exam01/src/main/java/kr/pmadvisor/persistence/MemberDAOImpl.java
+++ exam01/src/main/java/kr/pmadvisor/persistence/MemberDAOImpl.java
@@ -1,7 +1,8 @@
 package kr.pmadvisor.persistence;
 
+import javax.inject.Inject;
+
 import org.apache.ibatis.session.SqlSession;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import kr.pmadvisor.domain.MemberVO;
@@ -9,8 +10,9 @@ import kr.pmadvisor.domain.MemberVO;
 @Repository
 public class MemberDAOImpl implements MemberDAO{
 
-	@Autowired
+	@Inject
 	private SqlSession sqlSession;
+	
 	
 	private static final String namespace =
 			"kr.pmadvisor.mapper.MemberMapper";
---
+++ exam01/src/test/java/kr/pmadvisor/web/MemberDAOTest.java
@@ -0,0 +1,39 @@
+package kr.pmadvisor.web;
+
+import javax.inject.Inject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import kr.pmadvisor.domain.MemberVO;
+import kr.pmadvisor.persistence.MemberDAO;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/**/*.xml"})
+public class MemberDAOTest {
+	
+	@Inject
+	private MemberDAO dao; // MemberDAO를 주입함
+	
+	@Test
+	public void testTime() {
+		System.out.println(dao.getTime());		
+	}
+	
+	@Test
+	public void testInsertMember() {
+		
+		MemberVO vo = new MemberVO();
+		
+		vo.setUserid("user00");
+		vo.setUserpw("user00");
+		vo.setUsername("USER00");
+		vo.setEmail("user00@aaa.com");
+		
+		dao.insertMember(vo);
+		
+	}	
+
+}
